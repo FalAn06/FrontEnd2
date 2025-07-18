1950f6ff8f1d4a9f893d0555a8b042bd88ecab6f
FalAn06 <159930269+FalAn06@users.noreply.github.com>
2025-07-18 13:47:45 -0500

    feat: :sparkles: new structure

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,46 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode'; // Asegúrate de tener jwt-decode instalado
 
 const Cart = () => {
   const navigate = useNavigate();
   const [cartItems, setCartItems] = useState([]);
-  const [userId, setUserId] = useState(null);
-
-  // Decodificar el token para obtener el userId
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      try {
-        const decoded = jwtDecode(token);
-        setUserId(decoded.userId); // Asumiendo que el userId está en el payload del token
-      } catch (error) {
-        console.error('Error al decodificar el token', error);
-      }
-    }
-  }, []);
 
   // Obtener los productos del carrito
   useEffect(() => {
-    if (userId) {
-      const fetchCartItems = async () => {
-        try {
-          const response = await fetch(`http://98.85.200.29:5002/cart?userId=${userId}`);
-          const data = await response.json();
-          setCartItems(data.cart);
-        } catch (error) {
-          alert('Error al obtener el carrito');
-        }
-      };
+    const fetchCartItems = async () => {
+      try {
+        const response = await fetch('http://98.85.200.29:5002/cart');
+        const data = await response.json();
+        setCartItems(data.cart);
+      } catch (error) {
+        alert('Error al obtener el carrito');
+      }
+    };
 
-      fetchCartItems();
-    }
-  }, [userId]);
+    fetchCartItems();
+  }, []);
 
   // Función para eliminar un producto del carrito
   const handleRemoveFromCart = async (productId) => {
     try {
-      const response = await fetch(`http://98.85.200.29:5002/cart/${productId}?userId=${userId}`, {
+      const response = await fetch(`http://98.85.200.29:5002/cart/${productId}`, {
         method: 'DELETE',
       });
       const data = await response.json();
@@ -63,7 +46,6 @@ const Cart = () => {
           {cartItems.map(item => (
             <div key={item.productId} className="cart-item">
               <p>Producto ID: {item.productId}</p>
-              <p>Cantidad: {item.quantity}</p>
               <button onClick={() => handleRemoveFromCart(item.productId)} className="remove-button">
                 Eliminar
               </button>
